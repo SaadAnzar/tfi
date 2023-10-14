b935b3a0645ef898f728c6ae01067868177c198e
Md Anzar Ahmad <anzarhps@gmail.com>
2023-10-14 15:44:47 +0530

    add firebase config

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,20 +1,11 @@
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyAsbd3uVinEwb7UdDTmzpGhXLsulERzsaA',
-//   authDomain: 'bvv-tapas.firebaseapp.com',
-//   projectId: 'bvv-tapas',
-//   storageBucket: 'bvv-tapas.appspot.com',
-//   messagingSenderId: '789119516767',
-//   appId: '1:789119516767:web:f2ca6c78ea46c87adc1bea',
-//   measurementId: 'G-NCGW8CNQ8V',
-// }
-
 const firebaseConfig = {
-  apiKey: 'AIzaSyB03sIQGK_gwLoOliboRSK2173u5aV2Yo8',
-  authDomain: 'lesson-plan-generator-6704f.firebaseapp.com',
-  projectId: 'lesson-plan-generator-6704f',
-  storageBucket: 'lesson-plan-generator-6704f.appspot.com',
-  messagingSenderId: '306764491229',
-  appId: '1:306764491229:web:e17d0239c27e2674426e34',
+  apiKey: 'AIzaSyAXaZaiJ_IPpMVKZyaxFT-DpEtBzcGuaPw',
+  authDomain: 'dev1-358918.firebaseapp.com',
+  projectId: 'dev1-358918',
+  storageBucket: 'dev1-358918.appspot.com',
+  messagingSenderId: '359320062761',
+  appId: '1:359320062761:web:5d1ef8f6b2b0d9608f4538',
+  measurementId: 'G-YDLCJJQGN3',
 }
 
 export default firebaseConfig
